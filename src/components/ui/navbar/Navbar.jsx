9d0a08e6526860c15dff8509abe8ef8757c4ceac
Francisco Uribe <55062089+frankjobsmf@users.noreply.github.com>
2024-01-06 15:01:38 -0300

    update navbar

--- src/components/ui/navbar/Navbar.jsx
+++ src/components/ui/navbar/Navbar.jsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="">Crecer Libre WEB</Link>
+        <Link className="navbar-brand" to="">Crecer Libre</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
